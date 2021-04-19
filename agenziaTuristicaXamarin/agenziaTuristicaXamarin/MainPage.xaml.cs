f35db312694374e13d5c4f5de7d476fdc5650479
LiamBartolini <liambartolini@gmail.com>
2021-04-19 16:07:16 +0200

    creazione 'interfaccia' V0 aggiunta classi

--- agenziaTuristicaXamarin/agenziaTuristicaXamarin/MainPage.xaml.cs
+++ agenziaTuristicaXamarin/agenziaTuristicaXamarin/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using AS2021_TPSIT_4H_BartoliniLiam_AgenziaTuristica.Models;
 
 namespace agenziaTuristicaXamarin
 {
@@ -12,7 +14,23 @@ namespace agenziaTuristicaXamarin
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();   
+        }
+
+        private void Picker_Focused(object sender, FocusEventArgs e)
+        {
+            if (!pckAzioni.Items.Contains("Aggiungi escursione"))
+            {
+                pckAzioni.Items.Add("Aggiungi escursione");
+                pckAzioni.Items.Add("Modifica escursione");
+                pckAzioni.Items.Add("Elimina escursione");
+                pckAzioni.Items.Add("Aggiungi partecipante");
+                pckAzioni.Items.Add("Elimina partecipante");
+                pckAzioni.SelectedIndex = 0;
+            }
+
+            if (pckAzioni.SelectedIndex != -1)
+                Debug.WriteLine($"Scelta {pckAzioni.Items[pckAzioni.SelectedIndex]}");
         }
     }
 }
